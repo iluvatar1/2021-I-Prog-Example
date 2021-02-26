2fb11c0e06c48b04dbb1ea1e9b1aaf6dfa3e2571
William Oquendo <woquendo@gmail.com>
2021-02-26 15:24:40 +0000

    Mensaje dedwd d ewjkejkehrjkhwejkrj
    jfewjhjeh we ewhrejkwhrk erekj jkweh rehw
    werew rew  ewr ewwwwww rew

---
+++ hola.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // incluir la libreria de input output 
+
+// int argc char **argv para leer argumentos de la linea de comando
+int main(int argc, char **argv) 
+{
+  std::cout << "!Hola mundo!\n"; // \n caracter especial de nueva linea
+
+  return 0; // 0 errores al sistema operativo
+}
